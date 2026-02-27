2b3469ae26e0e92802bb9b19dae644fe74e4ed66
Stefcho69 <stefan08vss@gmail.com>
2026-02-27 23:29:48 +0200

    adding dto

---
+++ SMES-project/src/main/java/com/inventory_system/backend/dto/ProductDTO.java
@@ -0,0 +1,32 @@
+package com.inventory_system.backend.dto;
+
+public class ProductDTO {
+
+    private String name;
+    private int quality;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
