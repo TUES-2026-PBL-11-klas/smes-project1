dad5b6825c187399e710a7af9d81c8959c1b980f
Matrioshka <home2u.m.mateev@gmail.com>
2026-02-27 15:21:09 +0200

    fixing the models

--- inventory-system/backend/model/Product.java
+++ inventory-system/backend/model/Product.java
@@ -5,12 +5,14 @@ public class Product {
     private String name;
     private double price;
     private int quantity;
+    private String description;
 
-    public Product(int id, String name, double price, int quantity) {
+    public Product(int id, String name, double price, int quantity, String description ) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.description = description;
     }
 
     public int getId() { 
@@ -20,5 +22,6 @@ public class Product {
     public String getName() { return name; }
     public double getPrice() { return price; }
     public int getQuantity() { return quantity; }
+    public String getDescription() {return description; };
     public void setQuantity(int quantity) { this.quantity = quantity; }
 }
