4e27f2884042daa0be550733d1d383eb7f106716
Stefcho69 <stefan08vss@gmail.com>
2026-02-27 23:31:09 +0200

    adding repositories

---
+++ SMES-project/src/main/java/com/inventory_system/backend/repo/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.inventory_system.backend.repo;
+
+import com.inventory_system.backend.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,UUID> {
+    Optional<List<Product>> findAllByIsAddedFalse();
+}
